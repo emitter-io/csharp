ec3bdd938a1e50efd683c04e0fd394dcc917a871
Roman <kelindar@gmail.com>
2016-01-06 16:00:51 +0100

    license headers

--- Emitter/Emitter.cs
+++ Emitter/Emitter.cs
@@ -1,3 +1,17 @@
+/*
+Copyright (c) 2016 Roman Atachiants
+
+All rights reserved. This program and the accompanying materials
+are made available under the terms of the Eclipse Public License v1.0
+and Eclipse Distribution License v1.0 which accompany this distribution. 
+
+The Eclipse Public License:  http://www.eclipse.org/legal/epl-v10.html
+The Eclipse Distribution License: http://www.eclipse.org/org/documents/edl-v10.php.
+
+Contributors:
+   Roman Atachiants - integrating with emitter.io
+*/
+
 using System;
 using System.Collections;
 using Emitter.Network.Messages;
--- Emitter/Session/MqttSession.cs
+++ Emitter/Session/MqttSession.cs
@@ -1,17 +1,17 @@
 ﻿/*
+Copyright (c) 2016 Roman Atachiants
 Copyright (c) 2013, 2014 Paolo Patierno
 
 All rights reserved. This program and the accompanying materials
 are made available under the terms of the Eclipse Public License v1.0
 and Eclipse Distribution License v1.0 which accompany this distribution. 
 
-The Eclipse Public License is available at 
-   http://www.eclipse.org/legal/epl-v10.html
-and the Eclipse Distribution License is available at 
-   http://www.eclipse.org/org/documents/edl-v10.php.
+The Eclipse Public License:  http://www.eclipse.org/legal/epl-v10.html
+The Eclipse Distribution License: http://www.eclipse.org/org/documents/edl-v10.php.
 
 Contributors:
    Paolo Patierno - initial API and implementation and/or initial documentation
+   Roman Atachiants - integrating with emitter.io
 */
 
 using System.Collections;
