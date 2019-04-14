9a457891072a433bccb0b7472bfa088dd3c6fee3
Florimond Husquinet <florimond@husquinet.net>
2019-04-14 16:55:27 +0200

    Link

--- Emitter/Emitter.Link.cs
+++ Emitter/Emitter.Link.cs
@@ -12,19 +12,30 @@ namespace Emitter
             if (this.DefaultKey == null)
                 throw EmitterException.NoDefaultKey;
 
-            this.Link(this.DefaultKey, channel, isPrivate, subscribe, options);
+            this.Link(this.DefaultKey, channel, name, isPrivate, subscribe, options);
         }
 
         public void Link(string key, string channel, string name, bool isPrivate, bool subscribe, params string[] options)
         {
             var request = new LinkRequest();
             request.Key = key;
-            request.Channel = this.FormatChannel(null, channel, options);
+            request.Channel = this.FormatChannelLink(channel, options);
             request.Name = name;
             request.Private = isPrivate;
             request.Subscribe = subscribe;
 
-            this.Publish("emitter/", "presence", Encoding.UTF8.GetBytes(request.ToJson()));
+            this.Publish("emitter/", "link", Encoding.UTF8.GetBytes(request.ToJson()));
+        }
+
+        // TODO options
+        public ushort PublishWithLink(string name, byte[] message)
+        {
+            return this.Client.Publish(name, message);
+        }
+
+        public ushort PublishWithLink(string name, string message)
+        {
+            return this.Client.Publish(name, Encoding.UTF8.GetBytes(message));
         }
     }
 }
